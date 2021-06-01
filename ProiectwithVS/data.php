8e6fd25f7c4d7fb4a6554b7f58d09124060f0e92
Secarea <48117082+Secarea@users.noreply.github.com>
2021-06-01 03:47:27 -0700

    +AtentionareGeam

--- ProiectwithVS/data.php
+++ ProiectwithVS/data.php
@@ -2,9 +2,12 @@
 $conn=mysqli_connect("localhost", "root", "") or die(mysqli_error());
 mysqli_select_db($conn, "sensors");
 
-$sql_read = "SELECT * FROM sensorData";
+$sql_read1 = "SELECT avg(temperature) as avg_temp, avg(humidity) as avg_hum from sensorData";
+$result1= mysqli_query($conn, $sql_read1);
 
+$sql_read = "SELECT * FROM sensorData";
 $result = mysqli_query($conn, $sql_read);
+
 while($row = mysqli_fetch_array($result)) {
 	$id = $row['id'];
 	$temperature = $row['temperature'];
@@ -12,6 +15,12 @@ while($row = mysqli_fetch_array($result)) {
 	$door= $row['door'];
 	$created_at=$row['created_at'];
 }
+    
+while($row = mysqli_fetch_array($result1)) {
+    $average_temperature= $row['avg_temp'];
+    $average_humidity= $row['avg_hum'];
+}
+
 
 echo '<p>';
 echo '<i class="fas fa-thermometer-half"></i>';
@@ -39,3 +48,11 @@ if($door == 1){
     echo '</p>';	
     }
     echo '<p>Last update at:'. $created_at . '</p>';
+    #echo '<p>Average Temp:'. $average_temperature . '</p>';
+    #echo '<p>Average Humitiy:'. $average_humidity . '</p>';
+
+if( ($average_temperature < 22) && ( ($average_humidity >= 30) && ($average_humidity <= 50)) ){
+    echo '<p>Close the window!</p>';
+}else{
+    echo '<p>Open the window!</p>';
+}
